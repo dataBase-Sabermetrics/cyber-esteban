386127794ba67fe162bd18bba2d7a8bfe2f9f2b2
Andrew Beno <benodrew12@gmail.com>
2024-12-06 14:43:12 -0500

    fix handleRequests

--- main.go
+++ main.go
@@ -73,22 +73,22 @@ func newGame(w http.ResponseWriter, r *http.Request) {
 }
 
 func handleRequests() {
-	http.HandleFunc("/", homePage)
-	http.HandleFunc("/api/game", newGame)
-	go func() {
-		if err := http.ListenAndServe(":8080", nil); err != nil {
-			log.Fatal(err)
-		}
-	}()
+    http.HandleFunc("/", homePage)
+    http.HandleFunc("/api/game", newGame)
+
+    fmt.Println("Starting HTTP server on :8080...")
+    if err := http.ListenAndServe(":8080", nil); err != nil {
+        log.Printf("HTTP server error: %v\n", err)
+    }
 }
 
 func main() {
 	discord = startDiscord()
 	defer discord.Close()
 
-	handleRequests()
-	
 	fmt.Println("Bot is running")
+
+	go handleRequests()
 	
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
