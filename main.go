51d60b76c37be42e478c447bb1d581ddd6b98925
Andrew Beno <benodrew12@gmail.com>
2024-12-06 13:53:30 -0500

    Handle game starts

--- main.go
+++ main.go
@@ -4,47 +4,93 @@ import (
 	"fmt"
 	"log"
 	"os"
+  "io"
   "os/signal"
   "syscall"
 	"github.com/bwmarrin/discordgo"
 	"github.com/joho/godotenv"
+  "net/http"
 )
 
-func main() {
-  err := godotenv.Load()
-  if err != nil {
-    log.Fatal("Error loading .env file")
-  }
+var (
+	discord *discordgo.Session
+	channelID = "1313981819150143528"
+)
 
+func startDiscord() *discordgo.Session {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 	discordKey := os.Getenv("DISCORD_KEY")
+	sess, err := discordgo.New("Bot " + discordKey)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  sess, err := discordgo.New("Bot "+discordKey)
-  if err != nil {
-    log.Fatal(err)
-  }
+	sess.Identify.Intents = discordgo.IntentsAllWithoutPrivileged
 
-  sess.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
-    if m.Author.ID == s.State.User.ID {
-      return
-    }
+	sess.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
+		if m.Author.ID == s.State.User.ID {
+			return
+		}
+		if m.Content == "hello" {
+			s.ChannelMessageSend(m.ChannelID, "Hello, "+m.Author.Username)
+		}
+	})
 
-    if m.Content == "hello" {
-      s.ChannelMessageSend(m.ChannelID, "Hello, "+m.Author.Username)
-    }
-  })
+	err = sess.Open()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return sess
+}
 
-  sess.Identify.Intents = discordgo.IntentsAllWithoutPrivileged
+func homePage(w http.ResponseWriter, r *http.Request){
+    fmt.Fprintf(w, "Cyber Esteban is running 🚀")
+}
 
-  err = sess.Open()
-  if err != nil {
-    log.Fatal(err)
-  }
-  defer sess.Close()
+func newGame(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Only POST method is allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Error reading request body", http.StatusBadRequest)
+		return
+	}
+	gameMessage := string(body)
+	
+	_, err = discord.ChannelMessageSend(channelID, gameMessage)
+	if err != nil {
+		http.Error(w, "Error sending Discord message", http.StatusInternalServerError)
+		return
+	}
 
-  fmt.Println("Bot is running")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Received and sent to Discord: %s", gameMessage)
+}
 
-  sc := make(chan os.Signal, 1)
-  signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
-  <-sc
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/game", newGame)
+	go func() {
+		if err := http.ListenAndServe(":8080", nil); err != nil {
+			log.Fatal(err)
+		}
+	}()
+}
 
+func main() {
+	discord = startDiscord()
+	defer discord.Close()
+
+	handleRequests()
+	
+	fmt.Println("Bot is running")
+	
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-sc
 }
